a1b2bac96f6bbf6fc8f266c08892d514c4f308ae
app217 <osmangoni7542@gmail.com>
2026-06-25 21:52:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,8 @@ app.post("/sort-ticket", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
